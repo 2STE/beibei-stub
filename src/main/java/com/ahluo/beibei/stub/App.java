187e7ac20b8d4a5cd0df81e56808a35d6796c281
luochao <luozhongchaok@126.com>
2016-02-15 22:43:19 +0800

    initial

---
+++ src/main/java/com/ahluo/beibei/stub/App.java
@@ -0,0 +1,13 @@
+package com.ahluo.beibei.stub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
